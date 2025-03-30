f311af2201c51a08f5019bd2c272fdd21c3b76e8
Rizki-Rahmadani <rzkrhmdn7@gmail.com>
2025-03-30 21:32:50 +0700

    fixed: responsive ui for mobile at page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
       <Navbar />
       <div
         id=""
-        className="container-full h-screen flex flex-col md:flex-row justify-center items-center px-5 md:px-20 mt-60 sm:mt-0"
+        className="container-full h-screen flex flex-col md:flex-row justify-center items-center px-5 md:px-20 mt-58 sm:mt-0"
       >
         <div className="w-full md:w-1/2 flex flex-col justify-between text-justify md:text-start sm:text-start gap-5">
           <div className="space-y-3">
@@ -159,7 +159,7 @@ export default function Home() {
         </div>
 
         <div className="w-full md:w-1/2 h-auto m-10 flex justify-center md:justify-end relative">
-          <div className="absolute w-80 h-80 md:w-140 md:h-140 rounded-full bg-amber-600 -z-10 bottom-1  sm:-right-1"></div>
+          <div className="absolute w-90 h-90 md:w-140 md:h-140 rounded-full bg-amber-600 -z-10 bottom-1 sm:-right-1"></div>
           {/* <div className="absolute w-100 h-100 md:w-70 md:h-70 rounded-full bg-amber-600 -z-10 top-1 left-50"></div> */}
           <Image
             src="/assets/PP.png"
@@ -170,13 +170,13 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="mt-70 sm:mt-0">
+      <div className="mt-40 sm:mt-0">
         <PortfolioPage />
       </div>
       <div>
         <AboutMe />
       </div>
-      <div className="px-4 sm:px-6 md:px-8 lg:px-65 space-y-5">
+      <div className="mt-10 md:mt-0 px-4 sm:px-6 md:px-8 lg:px-65 space-y-5">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-center">
           CONTACT ME
         </h1>
